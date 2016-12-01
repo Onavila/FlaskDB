c04fb0290458a9c9b40ffef77403164afe0b8778
Jorge Trincado <jatrincado@uc.cl>
2016-12-01 16:13:44 -0300

    asd

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -52,7 +52,7 @@ def home():
 @app.route("/mongo")
 def mongo():
     query = request.args.get("query")
-    results = eval('mongodb.'+query)
+    results = eval('db.'+query)
     results = json_util.dumps(results, sort_keys=True, indent=4)
     if "find" in query:
         return render_template('mongo.html', results=results)
